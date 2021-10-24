ef1f3b349b2c31f3d9f71506f1e43e39a47b8c22
Francis Nguyen <francis@ngyn.me>
2021-10-23 21:12:17 -0600

    Hardcode image for Leaderboard

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -84,7 +84,15 @@ export default class Home extends Vue {
       beanTypes: ['leaderboard_season4'],
     }, '');
 
-    this.leaderboardS4 = results.docs.map((doc) => doc.data() as Bean);
+    this.leaderboardS4 = results.docs.map((doc) => {
+      const bean = doc.data() as Bean;
+      bean.imageUrl = 'https://firebasestorage.googleapis.com/v0/b/coffee-log-9bcc7.appspot.com/o/images%2FScreenshot%202021-10-23%20at%2021-08-38%20Leaderboard%20Coffee%20The%20Coffee%20Game.png?alt=media&token=f73b8ef5-0660-4f3a-9cb1-1357dd908afb';
+      return bean;
+    }).sort((a, b) => (
+      // Pure laziness; these beans are named "Coffee ##", so parse out the
+      // number and sort based on that.
+      // eslint-disable-next-line radix
+      parseInt(a.name.split(' ')[1]) < parseInt(b.name.split(' ')[1]) ? -1 : 1));
   }
 
   async updateRecentBeansAsync() {
